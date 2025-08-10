2863f3dc3752bc0fdb002b8962ea867d16d050c7
unknown <mdrafiqulakhter0@gmail.com>
2025-08-10 18:30:52 +0600

    after change

--- car-rental-server2/index.js
+++ car-rental-server2/index.js
@@ -15,12 +15,11 @@ const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:5174',
     'https://car-rental-76f12.web.app',
-    'https://car-rental-server-two-delta.vercel.app',
+    'https://car-rental-client-weld.vercel.app',
 
 ];
 
 app.use(cors({
-
 
     origin: function (origin, callback) {
 
